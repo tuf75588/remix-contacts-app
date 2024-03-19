ac866432b0e2c1f8bc58f90d2fb7719d173e9a96
Andrew Davis <develop.it4@gmail.com>
2024-03-19 14:09:44 -0400

    edit component more fleshed out

--- app/routes/contacts.$contactId_.edit.tsx
+++ app/routes/contacts.$contactId_.edit.tsx
@@ -1,9 +1,9 @@
-import type { LoaderFunctionArgs } from "@remix-run/node";
-import { json } from "@remix-run/node";
+import type { LoaderFunctionArgs, ActionFunctionArgs } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
 import invariant from "tiny-invariant";
 
-import { getContact } from "../data";
+import { getContact, updateContact } from "../data";
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
   invariant(params.contactId, "Missing contactId param");
@@ -14,6 +14,13 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
   return json({ contact });
 };
 
+export const action = async ({ params, request }: ActionFunctionArgs) => {
+  invariant(params.contactId, "Missing ContactId param");
+  const formData = await request.formData();
+  const updates = Object.fromEntries(formData);
+  await updateContact(params.contactId, updates);
+  return redirect(`/contacts/${params.contactId}`);
+};
 export default function EditContact() {
   const { contact } = useLoaderData<typeof loader>();
 
