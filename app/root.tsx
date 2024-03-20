4a2e802ac06cc4f3e236b1910bf6ce8e24b0c705
Andrew Davis <develop.it4@gmail.com>
2024-03-20 10:34:18 -0400

    URLSearchParams

--- app/root.tsx
+++ app/root.tsx
@@ -9,7 +9,7 @@ import {
   useNavigation,
   ScrollRestoration,
 } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
 import appStyleHref from "./app.css?url";
 import { createEmptyContact, getContacts } from "./data";
@@ -18,8 +18,10 @@ export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStyleHref },
 ];
 
-export const loader = async () => {
-  const contacts = await getContacts();
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
   return json({ contacts });
 };
 
